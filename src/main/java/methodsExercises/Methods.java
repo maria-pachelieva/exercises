d699dce861ac3709ef626d2036aad6ad3ddd1390
Maria <mariapachelieva@gmai.com>
2022-10-26 10:13:01 +0300

    test

---
+++ src/main/java/methodsExercises/Methods.java
@@ -0,0 +1,17 @@
+package methodsExercises;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void printMax(float number1,float number2) {
+
+        float max = number1;
+        if (number2 > number1){
+            max = number2;
+        }
+        System.out.println("Maximal number:"+ max);
+        }
+
+
+
+}
