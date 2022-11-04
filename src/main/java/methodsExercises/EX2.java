020f47c1290959a3cc44827b306c15458146a908
Maria <mariapachelieva@gmai.com>
2022-11-04 17:12:06 +0200

    SortingEngines

--- src/main/java/methodsExercises/EX2.java
+++ src/main/java/methodsExercises/EX2.java
@@ -14,15 +14,7 @@ public class EX2 {
     }
 
     public static int getMax(int num1, int num2) {
-        if (num1 > num2) {
-            System.out.printf("Max = %d", num1);
-            else if (num1 < num2) {
-                System.out.printf("Max = %d", num2);
-                else{
-                    System.out.printf("Numbers are equal.");
-                }
-            }
-        }
+        System.out.println(Math.max(num1, num2));
 
     }
 }
