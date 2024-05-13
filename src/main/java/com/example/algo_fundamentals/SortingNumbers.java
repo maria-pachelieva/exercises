7b17476236789c383fc79bb1776acb001b08cdec
Maria <mariapachelieva@gmai.com>
2024-05-13 17:18:34 +0300

    nestedLoops

---
+++ src/main/java/com/example/algo_fundamentals/SortingNumbers.java
@@ -0,0 +1,12 @@
+package com.example.algo_fundamentals;
+
+public class SortingNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sort(int... numbers) {
+
+        return numbers;
+    }
+}
