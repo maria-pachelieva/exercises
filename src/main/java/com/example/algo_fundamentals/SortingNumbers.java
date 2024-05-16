9ea5730c223702f05897ade94ee542120bc01b15
Maria <mariapachelieva@gmai.com>
2024-05-16 11:59:26 +0300

    nestedLoops

--- src/main/java/com/example/algo_fundamentals/SortingNumbers.java
+++ src/main/java/com/example/algo_fundamentals/SortingNumbers.java
@@ -2,11 +2,35 @@ package com.example.algo_fundamentals;
 
 public class SortingNumbers {
     public static void main(String[] args) {
+        int[] numbers = sort(10, 3, 5, -1, 0, 12, 8);
+        printNumbers(numbers);
 
     }
 
     public static int[] sort(int... numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int tempVar = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tempVar;
+
+                }
+            }
+
+        }
 
         return numbers;
     }
+
+    public static void printNumbers(int... numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%d", numbers[i]);
+            if (i < (numbers.length) - 1) {
+                System.out.printf(",");
+            }
+        }
+
+    }
+
 }
