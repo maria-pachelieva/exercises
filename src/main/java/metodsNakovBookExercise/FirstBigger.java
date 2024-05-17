17515734be7ffd8250f76584e8e96edc50d5dc59
Maria <mariapachelieva@gmai.com>
2024-05-17 17:23:59 +0300

    nestedLoops

--- src/main/java/metodsNakovBookExercise/FirstBigger.java
+++ src/main/java/metodsNakovBookExercise/FirstBigger.java
@@ -2,18 +2,20 @@ package metodsNakovBookExercise;
 
 public class FirstBigger {
     public static void main(String[] args) {
-        int arr[] = new int[]{1, 6, 7, 2, 5, 7, 23, 23, 6, 2, 3, 5, 1, 1, 2};
+        int arr[] = new int[]{1, 6, 0, 2, 5, 7, 23, 23, 6, 2, 3, 5, 1, 1, 2};
         firstElBigger(arr);
     }
 
     public static void firstElBigger(int arr[]) {
-        int arrNew [] = new int[6];
+        int number = 0;
         for (int i = 2; i < arr.length; i++) {
+
             if (arr[i] > arr[i - 1] && arr[i] > arr[i - 2]) {
-                arrNew[i-2] = i;
+                number = i;
+                break;
             }
         }
-        System.out.println(arrNew[0]);
+        System.out.println(number);
 
     }
 }
