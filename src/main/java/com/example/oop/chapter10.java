4ea815866ad0b51fad53cfc0b33f90cf83f1a9ac
Maria <mariapachelieva@gmai.com>
2024-06-12 15:55:07 +0300

    nestedLoops

---
+++ src/main/java/com/example/oop/chapter10.java
@@ -0,0 +1,13 @@
+package com.example.oop;
+
+public class chapter10 {
+    public static void main(String[] args) {
+        Cat[] arr = new Cat[10];
+        for (int i = 0; i++; i <10) {
+        Cat newCat = new Cat();
+        Cat[i] = newCat;
+        newCat.setName(String.valueOf(i));
+        }
+        System.out.println();
+    }
+}
